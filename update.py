3b937f13391f4f7e9291e9cc18cf0cd1ea298291
Alex Langhart <alex.langhart@jumpcloud.com>
2018-06-17 11:53:06 -0600

    Still not working

--- update.py
+++ update.py
@@ -2,6 +2,9 @@ import requests
 import csv
 import json
 import pprint
+import string
+import random
+from uuid import uuid4
 import pandas as pd
 import robobrowser
 import re
@@ -55,11 +58,14 @@ def prepare_upload_file(hare_file):
 
     modelmap = {}
     target_sum = 0
+    cash_index = None
     for index, row in df.iterrows():
         symbol = row['Security Symbol']
         target_pct = row['Target Percent']
         if not row['Security Symbol']:
             symbol = '$CASH'
+            cash_index = index
+            df.at[index, 'Security Symbol'] = symbol
         else:
             target_sum += target_pct
 
@@ -69,7 +75,7 @@ def prepare_upload_file(hare_file):
     cash_pct = round(100.0 - target_sum, 1)
     assert target_sum + cash_pct == 100.0, f'Target percents total {target_sum + cash_pct}'
     modelmap['$CASH']['targetPercent'] = cash_pct
-
+    df.at[cash_index, 'Target Percent'] = cash_pct
     model = {
         "modelsToAdd": [
             {
@@ -89,10 +95,10 @@ def prepare_upload_file(hare_file):
                 "isSubscribedModel": False
     }]}
 
-    return json.dumps(model)
+    return df.to_csv(index=False), json.dumps(model)
 
 
-def upload_hare_file(upload_file):
+def get_upload_cookies():
     username = 'smarek'
     password = '2$M@QMyDi!Z6Z5'
     chrome_options = Options()
@@ -102,14 +108,20 @@ def upload_hare_file(upload_file):
     driver.implicitly_wait(5)
     driver.get("https://www.advisorservices.com/")
     driver.find_element_by_name('USERID').clear()
-    driver.find_element_by_name('USERID').send_keys('smarek')
+    driver.find_element_by_name('USERID').send_keys(username)
     driver.find_element_by_id('password').clear()
-    driver.find_element_by_id('password').send_keys('2$M@QMyDi!Z6Z5')
+    driver.find_element_by_id('password').send_keys(password)
     driver.find_element_by_id('loginBtn').send_keys(Keys.ENTER)
     driver.switch_to.frame(0)
     driver.find_element(By.XPATH, '//*[@id="pageContainer"]/div[1]/table/tbody/tr/td[2]/table/tbody/tr/td[1]/a[7]').click()
     WebDriverWait(driver, 10).until(lambda d: len(d.window_handles) > 1)
-    driver.switch_to.window('irebal')
+    while True:
+        try:
+            driver.switch_to.window('irebal')
+            break
+        except:
+            time.sleep(0.1)
+
     print('Looking for loading overlay')
     WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, 'loading')))
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, 'CreateAndConfigure_text')))
@@ -121,7 +133,53 @@ def upload_hare_file(upload_file):
     driver.close()
     pprint.pprint(cookies)
     cookie_string = '; '.join([f'{x["name"]}={x["value"]}' for x in cookies])
+    return cookie_string
 
+
+def upload_csv(cookie_string, csv_data):
+    boundary = '---------------------------' + ''.join(random.choice(string.digits) for _ in range(27))
+    payload = f"""--{boundary}
+Content-Disposition: form-data; name="securityMdlDt"
+
+on
+--{boundary}
+Content-Disposition: form-data; name="modelFileUpload"
+
+upload.csv
+--{boundary}
+Content-Disposition: form-data; name="ImportModel"; filename="upload.csv"
+Content-Type: text/csv
+
+{csv_data}
+--{boundary}--
+"""
+    headers = {
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Cache-Control': 'max-age=0',
+            'Connection': 'keep-alive',
+            'Content-Length': str(len(payload)),
+            'Content-Type': f'multipart/form-data; boundary={boundary}',
+            'Cookie': cookie_string,
+            'Host': 'irebal-ct.advisorservices.com',
+            'Origin': 'https://irebal-ct.advisorservices.com',
+            'Referer': 'https://irebal-ct.advisorservices.com/irebal/model/inventory',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36',
+    }
+    print('Making request')
+
+    for k, v in headers.items():
+        print(f'{k}:{v}')
+    print(f'Payload:\n{payload}')
+    resp = requests.request('POST', 'https://irebal-ct.advisorservices.com/irebal/model/import=model', headers=headers,
+                            data=payload)
+    print(resp.text)
+    print(resp)
+
+
+def upload_json(cookie_string, json_data):
     headers = {
         'Accept': '*/*',
         'Accept-Encoding': 'gzip, deflate, br',
@@ -139,33 +197,13 @@ def upload_hare_file(upload_file):
     for k, v in headers.items():
         print(f'{k}:{v}')
     resp = requests.request('POST', 'https://irebal-ct.advisorservices.com/irebal/model/saveimport', headers=headers,
-                            data=upload_file)
-    print(resp.text)
-    print(resp)
-
-    headers = {
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Accept-Language': 'en-US,en;q=0.9',
-            'Cache-Control': 'max-age=0',
-            'Connection': 'keep-alive',
-            'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryH1tyvhqnxXElLQ0Z',
-            'Cookie': cookie_string,
-            'Host': 'irebal-ct.advisorservices.com',
-            'Origin': 'https://irebal-ct.advisorservices.com',
-            'Referer': 'https://irebal-ct.advisorservices.com/irebal/model/inventory',
-            'Upgrade-Insecure-Requests': '1',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.79 Safari/537.36',
-    }
-    print('Making request')
-    for k, v in headers.items():
-        print(f'{k}:{v}')
-    resp = requests.request('POST', 'https://irebal-ct.advisorservices.com/irebal/model/import=model', headers=headers)
+                            data=json_data)
     print(resp.text)
     print(resp)
 
 
 hare_file = download_hare_file()
-upload_file = prepare_upload_file(hare_file)
-print(upload_file)
-upload_hare_file(upload_file)
+csv_data, json_data = prepare_upload_file(hare_file)
+cookie_data = get_upload_cookies()
+upload_csv(cookie_data, csv_data)
+upload_json(cookie_data, json_data)
